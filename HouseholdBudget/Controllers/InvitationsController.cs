ff7bce25570e488a548fc4be1c86512fc680899d
dankin-code <dkinai@hotmail.com>
2016-02-11 16:02:08 -0500

    afternoon work

---
+++ HouseholdBudget/Controllers/InvitationsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HouseholdBudget.Models;
+
+namespace HouseholdBudget.Controllers
+{
+    public class InvitationsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Invitations
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Invitation.ToListAsync());
+        }
+
+        // GET: Invitations/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invitation invitation = await db.Invitation.FindAsync(id);
+            if (invitation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invitation);
+        }
+
+        // GET: Invitations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Invitations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,ToEmail,UserId")] Invitation invitation)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name).Id;
+                db.Invitation.Add(invitation);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(invitation);
+        }
+
+        // GET: Invitations/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invitation invitation = await db.Invitation.FindAsync(id);
+            if (invitation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invitation);
+        }
+
+        // POST: Invitations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,ToEmail,UserId")] Invitation invitation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(invitation).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(invitation);
+        }
+
+        // GET: Invitations/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invitation invitation = await db.Invitation.FindAsync(id);
+            if (invitation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invitation);
+        }
+
+        // POST: Invitations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Invitation invitation = await db.Invitation.FindAsync(id);
+            db.Invitation.Remove(invitation);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HouseholdBudget/Controllers/TransactionsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HouseholdBudget.Models;
+
+namespace HouseholdBudget.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Transactions
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Transaction.ToListAsync());
+        }
+
+        // GET: Transactions/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = await db.Transaction.FindAsync(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // GET: Transactions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Transactions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,TransactionDate,TransactionDescription,TransactionAmount,TransactionEnteredBy,TransactionType,Reconciled,ReconciledAmount,ReconciledById")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transaction.Add(transaction);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(transaction);
+        }
+
+        // GET: Transactions/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = await db.Transaction.FindAsync(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // POST: Transactions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,TransactionDate,TransactionDescription,TransactionAmount,TransactionEnteredBy,TransactionType,Reconciled,ReconciledAmount,ReconciledById")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(transaction).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(transaction);
+        }
+
+        // GET: Transactions/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = await db.Transaction.FindAsync(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // POST: Transactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Transaction transaction = await db.Transaction.FindAsync(id);
+            db.Transaction.Remove(transaction);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
