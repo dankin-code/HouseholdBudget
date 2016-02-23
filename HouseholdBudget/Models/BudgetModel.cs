335774b457aea2597cd498cd0346ab63ba7e41d7
dankin-code <dkinai@hotmail.com>
2016-02-23 12:15:50 -0500

    morning work

--- HouseholdBudget/Models/BudgetModel.cs
+++ HouseholdBudget/Models/BudgetModel.cs
@@ -114,16 +114,5 @@ namespace HouseholdBudget.Models
         public int HouseholdId { get; set; }
     }
 
-    public class DashboardViewModel
-    {
-        public int HouseholdId { get; set; }
-        public ICollection<Invitation> Invitations { get; set; }
-        public ICollection<Account> Accounts { get; set; }
-        public Budget CurrentBudget { get; set; }
-        public ICollection<BudgetItems> BudgetItemsList { get; set; }
-        public List<Transaction> Transactions { get; set; }
-    }
-
-
 
 }
